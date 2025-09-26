d3f0933a9e1d9a24fd60daf32dd0bb4c5a9f44aa
[dyad] <sundar@grabai.co.uk>
2025-09-26 18:33:36 +0100

    [dyad] Fixing the theme toggle button - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider attribute="class" defaultTheme="dark" storageKey="vite-ui-theme">
       <TooltipProvider>
         <Toaster />
         <Sonner />
